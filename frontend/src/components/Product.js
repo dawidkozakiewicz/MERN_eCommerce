0f2b5f7e0194caf36313c9c7e0f7a7ac7a3a5a14
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-12-14 09:32:32 +0100

    add router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,21 +3,17 @@ import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 const Product = ({ product }) => {
     return (
-        <Card className="my-3 p-3 rounded">
+        <Card className='my-3 p-3 rounded'>
             <a href={`/product/${product._id}`}>
-                <Card.Img variant="top" src={product.image} />
+                <Card.Img variant='top' src={product.image} />
             </a>
-            <Card.Title as="div">
+            <Card.Title as='div'>
                 <strong>{product.name}</strong>
             </Card.Title>
-            <Card.Text className='my-3' as="div">
-                <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} reviews`} />
+            <Card.Text className='my-3' as='div'>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             </Card.Text>
-            <Card.Text as="h3">
-                ${product.price}
-            </Card.Text>
+            <Card.Text as='h3'>${product.price}</Card.Text>
         </Card>
     )
 }
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import products from '../src/products'
+import Product from './components/Product'
+const HomeScreen = () => {
+    return (
+        <>
+            <h3>Latest products:</h3>
+            <Row>
+                {products.map((product) => {
+                    return (<Col sm={12} md={6} lg={4} xl={3}>
+                        <Product key={product._id} product={product} />
+                    </Col>)
+                })}
+            </Row>
+
+        </>
+    )
+}
+
+export default HomeScreen
