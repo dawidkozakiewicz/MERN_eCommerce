272e6e9e2408fffd6ee22ea6c61a89a68d3e9a0a
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-12-16 22:21:53 +0100

    replacing links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Shop Online</Navbar.Brand>
+                    <LinkContainer to='/'><Navbar.Brand>Shop Online</Navbar.Brand></LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i class="fas fa-user"></i>Sign in</Nav.Link>
+                            <LinkContainer to='/cart'><Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link></LinkContainer>
+                            <LinkContainer to='/login'><Nav.Link><i class="fas fa-user"></i>Sign in</Nav.Link></LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
+import { Link } from 'react-router-dom'
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img variant='top' src={product.image} />
-            </a>
-            <a href={`/product/${product._id}`}>
+            </Link>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as='div'>
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
             <Card.Text className='my-3' as='div'>
                 <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             </Card.Text>
