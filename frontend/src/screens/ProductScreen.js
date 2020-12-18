d56e3dca60d409eb988435426bfc8bd842b37502
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-12-18 22:43:14 +0100

    more details

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-const ProductScreen = () => {
+import Products from '../products'
+
+const ProductScreen = ({ match }) => {
+    const product = Products.find((p) => {
+        return p._id === match.params.id
+    })
+    console.log(product)
     return (
         <div>
-            product
+            {product.name}
         </div>
     )
 }
