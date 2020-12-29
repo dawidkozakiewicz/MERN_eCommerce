144b9b2d50a03548a3938c8b36e1597d4b311673
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-12-29 23:15:40 +0100

    strange things

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,11 +5,10 @@ import Product from './components/Product'
 const HomeScreen = () => {
     return (
         <>
-            <h3>Chuj</h3>
-            <h1>Hello</h1>
             <Row>
                 {products.map((product) => {
                     return (<Col sm={12} md={6} lg={4} xl={3}>
+                        <h1>Hello</h1>
                         <Product key={product._id} product={product} />
                     </Col>)
                 })}
