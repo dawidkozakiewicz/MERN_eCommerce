0f41ed2c48db7c64ed3eaa6d033af8b5e9c82181
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-12-31 19:09:01 +0100

    fixing wrong paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
-import HomeScreen from './HomeScreen'
+import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
 function App() {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../src/products'
-import Product from './components/Product'
+import products from '../products.js'
+import Product from '../components/Product'
 const HomeScreen = () => {
     return (
         <>
-            {/* <Row>
+            <Row>
                 {products.map((product) => {
                     return (<Col sm={12} md={6} lg={4} xl={3}>
                         <Product key={product._id} product={product} />
                     </Col>)
                 })}
-            </Row> */}
+            </Row>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ut ad quas. Officia nemo praesentium dolor alias modi, dignissimos pariatur, nostrum numquam molestias architecto, recusandae quas iure expedita dicta consequuntur.</h1>
         </>
     )
