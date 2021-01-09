f7adc2bf211c56d57aba2f5b66743970a0536a4f
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-01-09 23:08:52 +0100

    footter changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col className='text-center py-3'>
-                    Copyright &copy; Online Shop
+                    Copyright &copy; Online Shopp
                 </Col>
             </Row>
         </Container>
