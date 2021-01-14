d0f861fcf110d955190002e718fd940e57a27132
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-01-14 23:19:23 +0100

    ununderstandable object

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,6 +8,8 @@ const connectDB = async () => {
             useCreateIndex: true
         })
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
+        console.log(`Show me pure conn ${conn}`)
+        console.log(`Show me conn.connection ${conn.connection}`)
     } catch (error) {
         console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
