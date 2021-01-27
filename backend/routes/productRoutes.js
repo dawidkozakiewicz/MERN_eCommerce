ecb534376bb6c5a2897a2be04a5c0d73bcd2c921
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-01-27 18:32:36 +0100

    adding aggain asyncHandler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,29 +4,13 @@ import asyncHandler from 'express-async-handler'
 
 const router = express.Router()
 
-// router.get('/', asyncHandler(async (req, res) => {
-//     const products = await Product.find({})
-
-//     res.json(products)
-// }))
-
-// router.get('/:id', asyncHandler(async (req, res) => {
-//     const product = await Product.findById(req.params.id)
-
-//     if (product) {
-//         res.json(product)
-//     } else {
-//         res.status(404).json({ message: 'Product not found' })
-//     }
-// }))
-
-router.get('/', async (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({})
 
     res.json(products)
-})
+}))
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {
@@ -34,6 +18,22 @@ router.get('/:id', async (req, res) => {
     } else {
         res.status(404).json({ message: 'Product not found' })
     }
-})
+}))
+
+// router.get('/', async (req, res) => {
+//     const products = await Product.find({})
+
+//     res.json(products)
+// })
+
+// router.get('/:id', async (req, res) => {
+//     const product = await Product.findById(req.params.id)
+
+//     if (product) {
+//         res.json(product)
+//     } else {
+//         res.status(404).json({ message: 'Product not found' })
+//     }
+// })
 
 export default router
