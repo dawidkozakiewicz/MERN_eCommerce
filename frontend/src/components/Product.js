2ce9a2fa1eb6e648abbfe52f7697f019dde489df
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-02-07 00:01:47 +0100

    drunk but open-eyed

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,22 +3,22 @@ import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 import { Link } from 'react-router-dom'
 const Product = ({ product }) => {
-    return (
-        <Card className='my-3 p-3 rounded'>
-            <Link to={`/product/${product._id}`}>
-                <Card.Img variant='top' src={product.image} />
-            </Link>
-            <Link to={`/product/${product._id}`}>
-                <Card.Title as='div'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </Link>
-            <Card.Text className='my-3' as='div'>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-            </Card.Text>
-            <Card.Text as='h3'>${product.price}</Card.Text>
-        </Card>
-    )
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img variant='top' src={product.image} />
+      </Link>
+      <Link to={`/products/${product._id}`}>
+        <Card.Title as='div'>
+          <strong>{product.name}</strong>
+        </Card.Title>
+      </Link>
+      <Card.Text className='my-3' as='div'>
+        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+      </Card.Text>
+      <Card.Text as='h3'>${product.price}</Card.Text>
+    </Card>
+  )
 }
 
 export default Product
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -10,7 +10,7 @@ import Message from '../components/Message'
 const ProductScreen = ({ match }) => {
   const dispatch = useDispatch()
 
-  const productDetails = useSelector((state) => state.productDetails)
+  const productDetails = useSelector(state => state.productdetails)
   const { loading, error, product } = productDetails
   console.log(product)
   useEffect(() => {
@@ -27,60 +27,60 @@ const ProductScreen = ({ match }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid />
-          </Col>
-          <Col md={3}>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <h3>{product.name}</h3>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </ListGroup.Item>
-              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-              <ListGroup.Item>
-                Description: {product.description}
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-          <Col md={3}>
-            <Card>
-              <ListGroup variant='flush'>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Price:</Col>
-                    <Col>
-                      <strong>${product.price}</strong>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Status:</Col>
-                    <Col>
-                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Button
-                    className='btn-block'
-                    type='button'
-                    disabled={product.countInStock === 0}
-                  >
-                    Add To Cart
+            <Row>
+              <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid />
+              </Col>
+              <Col md={3}>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Rating
+                      value={product.rating}
+                      text={`${product.numReviews} reviews`}
+                    />
+                  </ListGroup.Item>
+                  <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                  <ListGroup.Item>
+                    Description: {product.description}
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col md={3}>
+                <Card>
+                  <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Price:</Col>
+                        <Col>
+                          <strong>${product.price}</strong>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                          {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Button
+                        className='btn-block'
+                        type='button'
+                        disabled={product.countInStock === 0}
+                      >
+                        Add To Cart
                   </Button>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card>
-          </Col>
-        </Row>
-      )}
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card>
+              </Col>
+            </Row>
+          )}
     </>
   )
 }
