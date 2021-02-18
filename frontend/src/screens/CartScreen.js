346dbd5e18e4375499172a0e3f523436e1e5d87b
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-02-18 20:24:44 +0100

    cartScreen with error

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import { Message } from '../components/Message'
+import Message from '../components/Message.js'
 import { addToCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
@@ -16,6 +16,7 @@ const CartScreen = ({ match, location, history }) => {
 
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
+    console.log(cartItems)
 
     useEffect(() => {
         if (productId) {
@@ -26,9 +27,27 @@ const CartScreen = ({ match, location, history }) => {
 
 
     return (
-        <Row md={8}>
-            Cart
-        </Row>
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 ? <Message>Your cart is empty <Link to='/'>Go Back</Link> </Message> : (
+                    <ListGroup variant='flush'>
+                        {cartItems.map((item) => {
+                            return (<ListGroup.Item key={Math.random()}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded></Image>
+                                        <p>PRODUCT</p>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>)
+                        })}
+                    </ListGroup>
+                )}
+            </Col>
+            <Col md={2}></Col>
+            <Col md={2}></Col>
+        </Row >
     )
 }
 
