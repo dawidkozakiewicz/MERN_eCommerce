3e828cfe05d0d88d42d71a69ae7603a47f039987
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-02-26 19:59:49 +0100

    user athentiaction endpoint

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -28,7 +28,7 @@ const HomeScreen = () => {
               {products.map((product) => {
                 return (
                   <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                    <Product key={product._id} product={product} />
+                    <Product product={product} />
                   </Col>
                 )
               })}
