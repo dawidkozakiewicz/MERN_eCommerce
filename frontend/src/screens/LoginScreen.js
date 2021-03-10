1a4bbb5680e1295edec9b62dee8ecc10b7b6794b
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-03-10 20:55:29 +0100

    adding FormContainer

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Message } from '../components/Message';
-import { Loader } from '../components/Loader';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
 
 import React from 'react'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
     return (
         <div>
 
