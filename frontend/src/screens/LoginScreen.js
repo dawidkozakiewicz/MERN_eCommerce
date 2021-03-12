6fd80b33aa856c173fafade8562d677bbabf5aa9
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-03-12 20:51:05 +0100

    login screen further work

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -15,9 +15,19 @@ const LoginScreen = () => {
 
 
     return (
-        <div>
-
-        </div>
+        <FormContainer>
+            <h1>Sign in</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                </Form.Group>
+            </Form>
+        </FormContainer>
     )
 }
 
