d843db5b83463d35af0be202fe2c222fcdd84233
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-03-13 23:17:42 +0100

    add button

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -26,6 +26,7 @@ const LoginScreen = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
+                <Button type='submit' variant='primary'></Button>
             </Form>
         </FormContainer>
     )
