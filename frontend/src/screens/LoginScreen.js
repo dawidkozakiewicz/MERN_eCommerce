8b2765e37deff38ddf3aac8e297d47aeab88259f
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-03-17 21:14:54 +0100

    finishing login page

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -2,27 +2,43 @@ import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { Message } from '../components/Message';
+import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
 
+
 import React from 'react'
 
-const LoginScreen = ({ location }) => {
+const LoginScreen = ({ location, history }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const { loading, error, userInfo } = userLogin
 
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
+    useEffect(() => {
+
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
+
+
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch(login(email, password))
     }
 
 
     return (
         <FormContainer>
             <h1>Sign in</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='email'>
                     <Form.Label>Email Address</Form.Label>
